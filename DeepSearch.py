79f335c8273c48b513f04cc120306192e815c366
dmg10007 <dalton.gifford@uconn.edu>
2017-12-15 11:28:44 -0500

    Update DeepSearch.py

--- DeepSearch.py
+++ DeepSearch.py
@@ -1,6 +1,7 @@
 pip install praw
 
 import praw
+import datetime
 
 reddit = praw.Reddit(client_id='rjzW-PmPp04b6g',
                      client_secret='...',
@@ -11,10 +12,10 @@ reddit = praw.Reddit(client_id='rjzW-PmPp04b6g',
 print("Welcome to Reddit Deep Search!")
 print("What can we help you find today?")
 
-post_type = input("Submission, Comment, Reply?")
+post_type = input("Submission, Comment, Reply? : ")
 
-date_entry = input('Enter a date in YYYY-MM-DD format')
+date_entry = input('Enter a date in YYYY-MM-DD format: ')
 year, month, day = map(int, date_entry.split('-'))
 date1 = datetime.date(year, month, day)
 
-sorted_by = input("Top, Newest, Oldest, Controversial?")
+sorted_by = input("Top, Newest, Oldest, Controversial? : ")
